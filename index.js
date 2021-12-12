de85426af01fdc216c7a395032bf4bccd02e4196
Nami Subagan <nami@Namis-iMac.local>
2021-12-12 20:18:51 +0800

    Added routes with hello paramater

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello from Nami Node 1')
 })
 
+app.get('/hello', (req, res) => {
+    res.send('I am with Hello!')
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log('Server running....'))
