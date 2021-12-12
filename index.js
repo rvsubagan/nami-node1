89a79cb77f0470e367acc1302d5ebe5365c899fc
Nami Subagan <nami@Namis-iMac.local>
2021-12-12 20:25:50 +0800

    Updated index.js reloaded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello from Nami Node 1')
+    res.send('Hello from Nami Node 1 reloaded!')
 })
 
 app.get('/hello', (req, res) => {
